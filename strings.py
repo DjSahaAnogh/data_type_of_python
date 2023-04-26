324c0cbc7d83beb526a915786386a156ef1e01a4
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2023-04-26 22:46:44 +0600

    Update

--- strings.py
+++ strings.py
@@ -1,9 +1,9 @@
 def get_strings_methods():
-  i: int = 0
-  for method in dir(str):
-    if "_" not in method:
-      i += 1
-      print(i, method, sep= " : ")
-      
-      
+    i: int = 0
+    for method in dir(str):
+        if "_" not in method:
+            i += 1
+            print(i, method, sep=" : ")
+
+
 get_strings_methods()
