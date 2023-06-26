84948bf5edb6bb14bd2ba6eddf7177577f021c93
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2023-06-26 14:03:32 +0600

    New codes added and edited the whole file.

--- strings.py
+++ strings.py
@@ -1,12 +1,12 @@
-def get_strings_methods():
-    i: int = 0
-    for method in dir(str):
-        if "_" not in method:
-            i += 1
-            print(i, method, sep=" : ")
-
-
-get_strings_methods()
+# def get_strings_methods():
+#     i: int = 0
+#     for method in dir(str):
+#         if "_" not in method:
+#             i += 1
+#             print(i, method, sep=" : ")
+#
+#
+# get_strings_methods()
 
 # 1. capitalize
 text = "hello EVERYONE"
@@ -43,35 +43,53 @@ text8 = "My name is DJ."
 print(text8.find("DJ"))
 
 # 9. format
-text9 = "{0}, My mane is {1}".format("Hello everyone!", "DJ")
-print(text9)
+text9 = "{0}, My mane is {1}"
+print(text9.format("Hello everyone!", "DJ"))
 
 # 10. format_map
 age: dict = \
     {"x": 45,
-     "y": 14}
+     "y": 10}
+text10 = "Joel is {x} and Elie is {y}"
+print(text10.format_map(age))
 
 # 11. index
-text10 = "Hello"
-index_num = text10.index("l")
+text11 = "Hello"
+index_num = text11.index("l")
 print(index_num)
 
 # 12. is_al_num
-text11 = "10"
-text12 = "Dj03"
-print(text11.isalnum())
+text12 = "10"
+text13 = "Dj-03"
 print(text12.isalnum())
+print(text13.isalnum())
 
 # 13. is_alpha
-text13 = "Hello"
-text14 = "10"
-print(text13.isalpha())
+text14 = "Hello"
+text15 = "10"
 print(text14.isalpha())
+print(text15.isalpha())
 
 # 14. is_a_scii
 text14 = "Hello"
-text15 = "HeLLė"
+text16 = "HeLLė"
 print(text14.isascii())
-print(text15.isascii())
+print(text16.isascii())
 
+# 15. is_decimal
+text17 = "123"
+print(text17.isdecimal())
 
+# 16. is_digit
+text18 = "①②③"
+text19 = "123"
+print(text18.isdigit())
+print(text19.isdigit())
+
+# 17. is_numeric
+text17 = "①②③"
+text18 = "123"
+text20 = "七六五"
+print(text17.isdigit())
+print(text18.isdigit())
+print(text20.isnumeric())
